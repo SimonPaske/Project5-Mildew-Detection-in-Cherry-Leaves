4612b81f166fd50e2bbb9083b50f757eaaa3ea4e
Simon Paske <simon.paske@icloud.com>
2024-01-21 20:47:01 +0100

    Prepare for deployment

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -1,14 +1,10 @@
-from PIL import Image
-from src.data_management import load_pkl_file
 import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import tensorflow as tf
 from tensorflow.keras.models import load_model
-
-keras = tf.keras
-models = tf.keras.models
+from PIL import Image
+from src.data_management import load_pkl_file
 
 
 def plot_predictions_probabilities(pred_proba, pred_class):
@@ -37,14 +33,12 @@ def plot_predictions_probabilities(pred_proba, pred_class):
 
 
 def resize_input_image(img, version):
-    """
-    Reshape image to average image size
-    """
-    image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
-    img_resized = img.resize(image_shape, Image.BICUBIC)
-    my_image = np.expand_dims(img_resized, axis=0)/255
+    target_size = (100, 100) if version == 'v1' else (224, 224)
+    img_resized = np.resize(img, target_size + (3,))
+    my_image = np.expand_dims(img_resized, axis=0) / 255
 
     return my_image
+
 
 
 def load_model_and_predict(my_image, version):
